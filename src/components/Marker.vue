8e010323702323e5a0235c1fcb962a4964b6bf7b
eric <erickmontes555@gmail.com>
2023-10-19 21:12:29 +0200

    add: marker component

---
+++ src/components/Marker.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import L from 'leaflet'
+import { onMounted, ref } from 'vue';
+
+const props = defineProps<{
+    map: L.Map,
+    latLng: L.LatLngExpression
+}>();
+
+const marker = ref();
+const markerElement = ref();
+
+onMounted(() => {
+    marker.value = L.marker(props.latLng).addTo(props.map);
+});
+
+</script>
+
+<template>
+    <div ref="markerElement"></div>
+</template>
