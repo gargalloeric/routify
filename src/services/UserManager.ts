acc11cbc80540efe6847f17a85f9d030943faf47
elbence <juanjopc08@gmail.com>
2023-11-16 18:39:44 +0100

    test(it01h01):fix skeleton and tests expectations

--- src/services/UserManager.ts
+++ src/services/UserManager.ts
@@ -1,18 +1,18 @@
 export class UserManager { // Singleton
 
-    private static userManager: UserManager;
-
-    private constructor() {
+    constructor() {
         // TODO
     }
 
-    static getManager(): UserManager {
-        if (!this.userManager) this.userManager = new UserManager()
-        return this.userManager;
-    }
-
-    register(name, email, password, repPassword) {
+    register(name, email, password, repPassword): string { // returns the mail of the user
         // TODO
         throw new Error("Not Implemented")
     }
+}
+
+let instance: UserManager;
+
+export function getUserManager(): UserManager {
+    if (!instance) instance = new UserManager()
+    return instance;
 }
