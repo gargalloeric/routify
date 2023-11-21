d26f8fe390ac5f44cb08c690958a6df5258acf2e
eric <erickmontes555@gmail.com>
2023-11-22 00:13:22 +0100

    refactor(it01h13): add distance extraction from the geoJSON object

--- src/model/Route.ts
+++ src/model/Route.ts
@@ -2,9 +2,10 @@ import {GeoJSON} from "leaflet";
 
 export class Route {
     puntos: GeoJSON;
-    distance: number = 0;
+    distance: number;
     constructor(puntos: GeoJSON) {
         this.puntos = puntos;
+        this.distance = puntos.features[0].properties.summary.distance / 1000;
     }
     getPuntos(): GeoJSON{
         return this.puntos;
