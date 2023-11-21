4ce2c69229864f2b4649ef8b6d95c6511ba87eff
eric <erickmontes555@gmail.com>
2023-11-21 22:12:06 +0100

    test(it01h13): add async/await to the scenario 1

--- tests/h-13.test.ts
+++ tests/h-13.test.ts
@@ -1,16 +1,16 @@
 import { expect, test } from "vitest";
-import { getRouteFromPlacesNames } from "../src/services/RouteManager";
-import {Transport} from "../src/model/Transport";
 import { Route } from "../src/model/Route";
+import { Transport } from "../src/model/Transport";
+import { getRouteFromPlacesNames } from "../src/services/RouteManager";
 
-test('getRoute_ApiOnline_routeIsObtained', () => {
+test('getRoute_ApiOnline_routeIsObtained', async () => {
     const origin = 'Castellón',
         destiny = 'Valencia',
         transport = Transport.Car;
 
-    const route: Route = getRouteFromPlacesNames(origin, destiny, transport);
-    expect(route.distance).toBeLessThan(1000);
-    expect(route.distance).toBeGreaterThan(30);
+    const route: Route = await getRouteFromPlacesNames(origin, destiny, transport);
+    expect(route.distance).resolves.toBeLessThan(1000);
+    expect(route.distance).resolves.toBeGreaterThan(30);
 
 })
 
