b93dbb13f2a62166c95c673ded8c6674e4fdfffd
eric <erickmontes555@gmail.com>
2023-11-22 00:15:03 +0100

    feat(it01h13): add adapter functions

--- src/services/ORSAdapter.ts
+++ src/services/ORSAdapter.ts
@@ -1,13 +1,17 @@
 // no - ponedlo en un paquete modelo o algo asi
 import { Route } from "../model/Route.ts";
 import {Transport} from "../model/Transport.ts";
+import { obtainCoordsFromName, obtainRoute } from "./ORS.ts";
 
-export function getRouteFromPlacesNames(origin: string, destiny: string, transport: Transport): Route {
-    // TODO
-    throw new Error("Not Implemented");
+export async function getRouteFromPlacesNames(origin: string, destiny: string, transport: Transport): Promise<Route> {
+        const coordsOrigin = await obtainCoordsFromName(origin);
+        const coordsDestiny = await obtainCoordsFromName(destiny);
+
+        const r = await obtainRoute(coordsOrigin, coordsDestiny, transport);
+        return new Route(r);
 }
 
-export function getRouteFromCoords(origin: L.LatLngExpression, destiny: L.LatLngExpression, transport: Transport): Route{
-    // TODO
-    throw new Error("Not Implemented");
+export async function getRouteFromCoords(origin: L.LatLngExpression, destiny: L.LatLngExpression, transport: Transport): Promise<Route> {
+    const r = await obtainRoute(origin, destiny, transport);
+    return new Route(r);
 }
