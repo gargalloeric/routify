41a67e102ce9cb93417f151e062fb902889d1975
mehdi <mehdigassabarsa@gmail.com>
2023-11-21 23:46:18 +0100

    feat(it01h13): first approach to UI

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <h1>Obtener una ruta entre dos lugares de interés</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #f0f0f0;
+  color: black;
+  padding: 10px;
+  text-align: center;
+  width: 100%;
+}
+
+h1 {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  margin: 0;
+}
+</style>
+
+
+
