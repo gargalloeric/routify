d018f1b6d84a1ea78e4380732cc6b5608ab6d120
eric <erickmontes555@gmail.com>
2023-11-22 00:16:22 +0100

    test(it01h13): remove unnecesary async/await stuff and added more specific place names

--- tests/h-13.test.ts
+++ tests/h-13.test.ts
@@ -4,17 +4,17 @@ import {Transport} from "../src/model/Transport";
 import { Route } from "../src/model/Route";
 
 test('getRoute_ApiOnline_routeIsObtained', async () => {
-    const origin = 'Castellón',
-        destiny = 'Valencia',
+    const origin = 'Castellón de la plana',
+        destiny = 'Valencia, España',
         transport = Transport.Car;
 
     const route: Route = await getRouteFromPlacesNames(origin, destiny, transport);
-    expect(route.distance).resolves.toBeLessThan(1000);
-    expect(route.distance).resolves.toBeGreaterThan(30);
+    expect(route.distance).toBeLessThan(1000);
+    expect(route.distance).toBeGreaterThan(30);
 
 })
 
-test("getRoute_ApiOnlineAndPlaceNameNotValid_ThorwPlaceNotValidException", () => {
+test("getRoute_ApiOnlineAndPlaceNameNotValid_ThorwPlaceNotValidException", async () => {
     const origin = 'Castellón',
         destiny = 'jldhkjha',
         transport = Transport.Car;
