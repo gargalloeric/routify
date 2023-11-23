21e50c7de7b798b6522a22d514c58e56e11408dd
elbence <juanjopc08@gmail.com>
2023-11-23 10:27:02 +0100

    test(it01h02): fix test values to eliminate dependencies between test executions

--- tests/h-02.test.ts
+++ tests/h-02.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import {UserManager, getUserManager} from "../src/services/UserManager";
 
 test('logInUser_ValidInputs_logInUser', async () => {
-    let email: string = 'jose@gmail.com',
+    let email: string = 'edu.jose@gmail.com',
         password: string = 'aS0@28Y?';
 
     let mail = await getUserManager().logIn(email, password)
@@ -10,8 +10,8 @@ test('logInUser_ValidInputs_logInUser', async () => {
 })
 
 test('logInUser_InvalidPassword_notLogInUser', async () => {
-    let email: string = 'jose@gmail.com',
-        password: string = 'S438w'
+    let email: string = 'inexistente.jose@gmail.com',
+        password: string = 'S438w?'
 
     await expect(() => getUserManager().logIn(email, password)).rejects.toThrowError('Password not valid')
 })
