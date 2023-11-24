50284a3555cae449de5ed4b68fb088d9e0026fbc
elbence <juanjopc08@gmail.com>
2023-11-24 15:52:47 +0100

    fix(it01h01): remove todo from firebase utils

--- src/services/FirebaseUtils.ts
+++ src/services/FirebaseUtils.ts
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
+// Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
