a11381e70cca12c1da1b8b272dcce26800c55e46
eric <erickmontes555@gmail.com>
2023-11-25 21:27:57 +0100

    refactor(it01h13): add origina and destiny properties to Route.

--- src/model/Route.ts
+++ src/model/Route.ts
@@ -4,8 +4,12 @@ export class Route {
     geoJSON: L.GeoJSON;
     distance: number;
     transport: Transport;
-    constructor(geoJSON: L.GeoJSON) {
+    origin: string;
+    destiny: string;
+    constructor(geoJSON: L.GeoJSON, origin: string, destiny: string) {
         this.geoJSON = geoJSON;
+        this.origin = origin;
+        this.destiny = destiny;
         this.distance = geoJSON.features[0].properties.summary.distance / 1000;
         this.transport = Transport[geoJSON.metadata.profile as keyof  typeof Transport];
     }
