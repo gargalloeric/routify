e27e7fefa09a22667b089e4892ec3c8698b3318d
eric <erickmontes555@gmail.com>
2023-11-25 21:28:35 +0100

    feat(it01h13) add funtion to obtain name from coords

--- src/services/ORS.ts
+++ src/services/ORS.ts
@@ -3,11 +3,12 @@ import { Transport } from "../model/Transport";
 const BASE_URL = "https://api.openrouteservice.org";
 
 const URL_GEOCODE_SEARCH = "/geocode/search?";
+const URL_GEOCODE_REVERSE = "/geocode/reverse?";
 const SEARCH_RESULT_LIMIT = 1;
 
 const URL_DIRECTIONS = "/v2/directions/";
 
-export async function obtainCoordsFromName(placeName: string): Promise<L.LatLngExpression> {
+export async function obtainCoordsFromName(placeName: string): Promise<JSON> {
     const target = new URL(URL_GEOCODE_SEARCH, BASE_URL);
     const resp = await fetch(target.toString() + new URLSearchParams({
         api_key: import.meta.env.VITE_ORS_API,
@@ -21,10 +22,27 @@ export async function obtainCoordsFromName(placeName: string): Promise<L.LatLngE
         throw new Error("Place name not valid");
     }
 
-    const {coordinates} = data.features[0].geometry;
-    return coordinates;
+    return data.features[0];
 
 
+}
+
+export async function obtainNameFromCoords(coords: L.LatLng): Promise<JSON> {
+    const { lat, lng } = coords
+    const target = new URL(URL_GEOCODE_REVERSE, BASE_URL);
+    const resp = await fetch(target.toString() + new URLSearchParams({
+        api_key: import.meta.env.VITE_ORS_API,
+        'point.lon': lng.toString(),
+        'point.lat': lat.toString(),
+        size: SEARCH_RESULT_LIMIT.toString()
+    }));
+
+    const data = await resp.json();
+    if (data.features.length === 0) {
+        throw new Error("Place name not known");
+    }
+
+    return data.features[0];
 }
 
 export async function obtainRoute(start: L.LatLngExpression, end: L.LatLngExpression, transport: Transport): Promise<L.GeoJSON> {
