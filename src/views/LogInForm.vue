5db170df4a44f13eac67169b5af7cd9ab49c456d
elbence <juanjopc08@gmail.com>
2023-11-30 18:30:50 +0100

    refactor(it01): restyle logIn form using tailwind

--- src/views/LogInForm.vue
+++ src/views/LogInForm.vue
@@ -21,29 +21,25 @@ async function submitRegistration() {
 </script>
 
 <template>
-  <div class="central-container">
-    <h1>LogIn</h1>
-    <table>
-      <tr>
-        <td class="text-right">Mail:</td>
-        <td><input v-model="mail" placeholder="john@apple.example" /></td>
-      </tr>
-      <tr>
-        <td class="text-right">Password:</td>
-        <td><input type="password" v-model="passwd" placeholder="*****" /></td>
-      </tr>
-      <tr>
-        <td colspan="2" class="error-message">{{errorMessage}}</td>
-      </tr>
-      <tr>
-        <td colspan="2"><button @click="submitRegistration">Log In</button></td>
-      </tr>
-      <tr>
-        <td colspan="2"><span>Don't have an account? <span class="link" @click="router.push({path: '/register'})">Register</span></span></td>
-      </tr>
-    </table>
+  <div class="max-w-sm mx-auto">
+    <h1 class="font-bold text-2xl mb-4">Inicia sesion:</h1>
+    <div class="mb-5">
+      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Tu correo electrónico</label>
+      <input type="email" v-model="mail" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="johnapple@gmail.com" required>
+    </div>
+    <div class="mb-5">
+      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Tu contraseña</label>
+      <input type="password" v-model="passwd" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+    </div>
+    <p class="mb-4 ms-2 text-sm font-medium text-red-600 dark:text-gray-300">
+      {{errorMessage}}</p>
+    <p class="mb-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+      ¿No tienes cuenta? <span class="link" @click="router.push({path: '/register'})">Registrate</span></p>
+    <button @click="submitRegistration" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      Inicia Sesión</button>
   </div>
-
 </template>
 
 <style scoped>
