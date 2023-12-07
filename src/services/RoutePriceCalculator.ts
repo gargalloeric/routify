61bba796640b480ebf284f55318d019b8b20a227
elbence <juanjopc08@gmail.com>
2023-12-07 20:25:59 +0100

    test(it02h14): implemented base acceptance tests and other necessary skel implementation

---
+++ src/services/RoutePriceCalculator.ts
@@ -0,0 +1,6 @@
+import {Route} from "../model/Route.ts";
+import {Vehicle} from "../model/Vehicle.ts";
+
+export function calculateRoutePriceWithCar(route: Route, vehicle: Vehicle): number {
+    throw new Error("Not Implemented")
+}
---
+++ tests/h-14.test.ts
@@ -0,0 +1,61 @@
+import { expect, test } from "vitest";
+import {getUserManager} from "../src/services/UserManager";
+import {Transport} from "../src/model/Transport";
+import {Route} from "../src/model/Route";
+import {getRouteFromPlacesNames} from "../src/services/ORSAdapter";
+import {Vehicle} from "../src/model/Vehicle";
+import {calculateRoutePriceWithCar} from "../src/services/RoutePriceCalculator";
+
+
+// E01 -    Válido
+// Given:   el usuario esta loggeado
+// And:     elige una ruta de X km de distancia
+// And:     tiene un vehiculo {“nombre”: “nave galáctica”, “tipo-motor”: “combustión”, “consumo-100-km”: “5”}
+// When:    se quiere conocer el precio de la ruta con el coche de combustión “nave galactica”.
+// Then:    La api externa devuelve un precio de gasolina de *ej:Y €/ L
+// And:     Se calcula y se muestra el precio *ej:7,5€.
+test('obtainRouteCost_UserHasVehicleCostApiOnline_ObtainCost', async () => {
+    // given - log user
+    const email: string = 'edu2.jose@notamail.not', password: string = 'aS0@28Y?';
+    const userManager = getUserManager();
+    await userManager.logIn(email, password);
+
+    // given - create route +/-100km?
+    const origin = 'Castellón de la plana', destiny = 'Valencia, España', transport = Transport.Car;
+    const route: Route = await getRouteFromPlacesNames(origin, destiny, transport); // a route between 50 and 100 km
+
+    // given - user has vehicle
+    const matricula: string = "1212XLX", nombre: string = "nave galáctica", tipoMotor: string = "combustión", consumo100Km: number = 5;
+    await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km).catch(() => {});
+    const vehicle: Vehicle = userManager.getUserVehicle(matricula)
+
+    // tests methods
+    const price: number = calculateRoutePriceWithCar(route, vehicle)
+    expect(price).toBeLessThan(20)
+    expect(price).toBeGreaterThan(1)
+
+    // cleanup - remove user - logOut
+    userManager.logOut()
+})
+
+// E03 -    Inválido
+// Given:   el usuario de la aplicación no ha elegido ninguna ruta.
+// When:    se quiere conocer el precio de la ruta con un vehículo de combustión.
+// Then:    se muestra la excepción InvalidRouteException.
+test('obtainRouteCost_NoRouteSelected_ThrowsInvalidRouteException', async () => {
+    // given - log user
+    const email: string = 'edu2.jose@notamail.not', password: string = 'aS0@28Y?';
+    const userManager = getUserManager();
+    await userManager.logIn(email, password);
+
+    // given - user has vehicle
+    const matricula: string = "1212XLX", nombre: string = "nave galáctica", tipoMotor: string = "combustión", consumo100Km: number = 5;
+    await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km).catch(() => {});
+    const vehicle: Vehicle = userManager.getUserVehicle(matricula)
+
+    // tests methods
+    await expect(() => calculateRoutePriceWithCar(null, vehicle)).rejects.toThrowError('Invalid route')
+
+    // cleanup - remove user - logOut
+    userManager.logOut()
+})
