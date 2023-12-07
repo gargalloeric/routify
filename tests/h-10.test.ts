6024cffeb6b04e2534d852c60aa60c8f70afa5f4
eric <erickmontes555@gmail.com>
2023-12-07 18:08:49 +0100

    test(it02h10): change user and handle exception if vehicles already exists

--- tests/h-10.test.ts
+++ tests/h-10.test.ts
@@ -3,7 +3,7 @@ import { getUserManager } from "../src/services/UserManager";
 
 test('getListOfVehicles_TwoVehiclesStored_ObtainListOfVehicles', async () => {
     // Given: the user is logged in
-    let email: string = 'edu.jose@gmail.com',
+    let email: string = 'edu2.jose@notamail.not',
         password: string = 'aS0@28Y?';
 
     const userManager = getUserManager();
@@ -12,31 +12,33 @@ test('getListOfVehicles_TwoVehiclesStored_ObtainListOfVehicles', async () => {
     // And: There are two vehicles registered
     const matricula1: string = "1212XLX",
     nombre1: string = "nave galáctica",
-    tipoMotor1: string = "combustion",
+    tipoMotor1: string = "combustión",
     consumo100Km1: number = 5;
 
     const matricula2: string = "1414XLX",
     nombre2: string = "tractor amarillo",
-    tipoMotor2: string = "combustion",
+    tipoMotor2: string = "combustión",
     consumo100Km2: number = 20;
 
-    await userManager.registerVehicle(matricula1, nombre1, tipoMotor1, consumo100Km1);
-    await userManager.registerVehicle(matricula2, nombre2, tipoMotor2, consumo100Km2);
+    // Try to register the vehicles if they don't exist
+    await userManager.registerVehicle(matricula1, nombre1, tipoMotor1, consumo100Km1).catch(() => console.log('Already registered'));
+    await userManager.registerVehicle(matricula2, nombre2, tipoMotor2, consumo100Km2).catch(() => console.log('Already registered'));
 
     // When: access the list of vehicles
-    const listOfVehicles = await userManager.getListOfVehicles();
+    const listOfVehicles = userManager.getListOfVehicles();
 
     // Then: a list of the stored vehicles is obtained
-    expect(listOfVehicles.length).toBe(2);
-    expect(listOfVehicles[0].matricula).toBe("1212XLX");
-    expect(listOfVehicles[1].matricula).toBe("1414XLX");
+    expect(Object.keys(listOfVehicles).length).toBe(2);
+    expect(listOfVehicles['1212XLX']).toBeDefined();
+    expect(listOfVehicles['1414XLX']).toBeDefined();
 })
 
-test('getListOfVehicles_UserNotLoggedIn_ThrowUserNotLoggedInException', async () => {
+test('getListOfVehicles_UserNotLoggedIn_ThrowUserNotLoggedInException', () => {
     // Given: the user is not logged in
     const userManager = getUserManager();
+    userManager.userInfo = null;
 
     // When: access the list of vehicles
     // Then: An error is thrown
-    await expect(() => { userManager.getListOfVehicles() }).rejects.toThrowError('User not logged in');
+    expect(() => { userManager.getListOfVehicles() }).toThrowError('User must be logged in to list vehicles');
 })
