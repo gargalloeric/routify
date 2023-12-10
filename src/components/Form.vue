7d264675ff3a3c6ac77acfdb1b9100602e96a2c6
mehdi <mehdigassabarsa@gmail.com>
2023-12-10 18:49:17 +0100

    fix(it02h14): form inputs fixed when empty.

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -73,7 +73,7 @@ function getRoute() {
       <input v-if="!errorInOrigin" v-model="formRoute.origin" type="text" id="origin"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Madrid">
-      <input v-if="errorInOrigin"  v-model="formRoute.destination" type="text" id="destiny" class="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400"  placeholder="Madrid">
+      <input v-if="errorInOrigin"  v-model="formRoute.origin" type="text" id="destiny" class="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400"  placeholder="Madrid">
       <p v-if="errorInOrigin" class="mt-2 text-sm text-red-600 dark:text-red-500">Origen vacío.</p>
     </div>
     <div class="mb-5">
