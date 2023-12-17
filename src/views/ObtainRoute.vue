70ee0e6b8179b89979992711abe5fcf447b54f3f
eric <erickmontes555@gmail.com>
2023-12-17 19:49:47 +0100

    refactor(it03hu16): handle type of route

--- src/views/ObtainRoute.vue
+++ src/views/ObtainRoute.vue
@@ -9,6 +9,7 @@ import {latLng} from "leaflet";
 import {calculateRoutePriceWithCar} from "../services/RoutePriceCalculator.ts";
 import {isPriceRequested} from "../main.ts";
 import {Vehicle} from "../model/Vehicle.ts";
+import { RouteType } from "../model/Route";
 
 // TODO: Make initialLatLang the user location or a default coordinates fallback.
 const initLatLang: L.LatLngExpression = [39.98541896850344, -0.05080976072749943];
@@ -17,14 +18,14 @@ const map = ref();
 const isRequestingRoute = ref(false);
 const isRequestReturnedError = ref(false);
 
-async function handleRouteRequest(data: { origin: any, destination: any, mode: Transport, vehicle: Vehicle}) {
+async function handleRouteRequest(data: { origin: any, destination: any, mode: Transport, vehicle: Vehicle, type: RouteType}) {
   isRequestingRoute.value = true;
   let route;
   try {
     if (/^[A-Za-z]/.test(data.origin.toString()))
-      route = await getRouteFromPlacesNames(data.origin.toString(), data.destination.toString(), data.mode);
+      route = await getRouteFromPlacesNames(data.origin.toString(), data.destination.toString(), data.mode, data.type);
     else
-      route = await getRouteFromCoords(latLng(data.origin), latLng(data.destination), data.mode)
+      route = await getRouteFromCoords(latLng(data.origin), latLng(data.destination), data.mode, data.type);
     map.value.clear();
     map.value.drawRoute(route);
     if (data.vehicle != undefined){
