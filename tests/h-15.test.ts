d86fa00cd6b353f77f2e96f6d001d76ad3629b56
eric <erickmontes555@gmail.com>
2023-12-23 16:54:01 +0100

    test(it04hu15): change vehicle for consumption/100

--- tests/h-15.test.ts
+++ tests/h-15.test.ts
@@ -1,22 +1,22 @@
 import { expect, test } from "vitest";
 import { getRouteFromPlacesNames } from "../src/services/ORSAdapter";
 import { Transport } from "../src/model/Transport";
-import { Route, RouteType } from "../src/model/Route";
+import { RouteType } from "../src/model/Route";
 
 test('calculateRoutePrice_bicycleVehicle_ObtainCost', async () => {
     const origin = 'Castellón de la plana';
     const destiny = 'Valencia, España';
-    const vehile = new Bycicle('mi bicicleta', 2200);
+    const consumptionAt100 = 2200;
 
     const route = await getRouteFromPlacesNames(origin, destiny, Transport.Bycicle, RouteType.Recommended);
 
-    const cost = calculateRoutePrice(route, vehile, new BycicleCostStartey());
+    const cost = calculateRoutePrice(route, consumptionAt100, new BycicleCostStartey());
 
     expect(cost).toBeCloseTo(1633);
 })
 
 test('calculateRoutePrice_emptyRoute_throwInvalidRouteException', () => {
-    const vehicle = new OnFoot('walking', 2200);    
+    const consumptionAt100 = 2200;
 
-    expect(calculateRoutePrice(null, vehicle, new FootCostStartey())).toThrowError('Invalid route');
+    expect(calculateRoutePrice(null, consumptionAt100, new FootCostStartey())).toThrowError('Invalid route');
 })
