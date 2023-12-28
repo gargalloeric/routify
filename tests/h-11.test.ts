44c166418ec14805cd017465e8264b0e2adbe3dd
eric <erickmontes555@gmail.com>
2023-12-28 17:04:18 +0100

    test(it04hu11): add acceptance tests

---
+++ tests/h-11.test.ts
@@ -0,0 +1,47 @@
+import { test, expect } from "vitest";
+import { getUserManager } from "../src/services/UserManager";
+
+test('deleteVehicle_UserLoggedInVehicleExists_VehicleDeleted', async () => {
+    const email: string = 'edu.jose@gmail.com',
+        password: string = 'aS0@28Y?';
+
+    const matricula: string = "1414XLX",
+        nombre: string = "nave galáctica",
+        tipoMotor: string = "combustión",
+        consumo100Km: number = 5;
+
+    const userManager = getUserManager();
+    await userManager.logIn(email, password);
+    await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km);
+
+    let result = await userManager.deleteVehicle(matricula);
+    expect(result).toBe(true);
+
+    // We can't delete a vehicle that doesn't exists
+    result = await userManager.deleteVehicle(matricula);
+    expect(result).toBe(false);
+
+})
+
+test('deleteVehicle_UserNotLoggedInVehicleExists_ThrowUserNotLoggedInException', async () => {
+    const email: string = 'edu.jose@gmail.com',
+        password: string = 'aS0@28Y?';
+
+    const matricula: string = "1414XLX",
+        nombre: string = "nave galáctica",
+        tipoMotor: string = "combustión",
+        consumo100Km: number = 5;
+
+    const userManager = getUserManager();
+    await userManager.logIn(email, password);
+    await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km);
+
+    userManager.userInfo = null;
+    await expect(userManager.deleteVehicle(matricula)).rejects.toThrowError('User not logged in');
+
+    // We log in again and delete the vehicle
+    await userManager.logIn(email, password);
+    let result = await userManager.deleteVehicle(matricula);
+    expect(result).toBe(true);
+
+})
