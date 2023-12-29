7eeb3cbda29fe6de313bd62c1a8fbe421d458d29
mehdi <mehdigassabarsa@gmail.com>
2023-12-30 00:21:08 +0100

    test(it04h05): acceptance test e06 implemented

--- tests/h-05.test.ts
+++ tests/h-05.test.ts
@@ -17,9 +17,25 @@ test('registerPlaceFromPlaceCoords_UserRegisteredDBAvailablePlaceApiAvailableVal
     // tests methods
     const userManager = getUserManager();
     await userManager.logIn(email, password);
-
+    await getUserManager().deletePlaceCoords(placeCoords);
 
     const registered = await userManager.registerPlaceFromPlaceCoords(placeCoords);
     expect(registered).toBe(true);
-    await getUserManager().deletePlaceCoords(placeCoords);
-})
+});
+
+// E06 - Inválido
+// Given: hay algún lugar de interés almacenado InterestLocations = [ {“latitude” : “39.99447958251366”, “longitude” : “-0.07022543179411124”, “place_name”: “Castellón de la Plana”}].
+// When: se intenta almacenar un lugar de interés ya almacenado “(39.99447958251366, -0.07022543179411124)”
+// Then: se lanza la excepción CoordinatesAlreadyStoredException.
+
+test('registerPlaceFromPlaceCoords_UserRegisteredPlaceApiAvailablePlaceAlreadyStored_throwCoordinatesAlreadyStoredException', async () => {
+    const email: string = 'edu.jose@gmail.com',
+        password: string = 'aS0@28Y?';
+
+    const placeCoords : L.LatLng = [39.99447958251366, -0.07022543179411124];
+    // tests methods
+    const userManager = getUserManager();
+    await userManager.logIn(email, password);
+
+    await expect(() => userManager.registerPlaceFromPlaceCoords(placeCoords)).rejects.toThrowError('Coordinates already stored');
+});
