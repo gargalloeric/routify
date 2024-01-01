65a96bba2cfdd1297865436f345b2a5a21b9f1ea
mehdi <mehdigassabarsa@gmail.com>
2024-01-01 17:45:31 +0100

    fix(it04h05): completed acceptance test e02

--- tests/h-12.test.ts
+++ tests/h-12.test.ts
@@ -22,8 +22,35 @@ test('updateVehicle_UserRegisteredDBAvailableValidInputs_UpdateVehicle', async (
     await getUserManager().deleteVehicle(matricula).catch(() => {})
     await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km).catch(() => {});
 
-    let vehicle : Vehicle = userManager.getUserVehicle(matricula);
-    expect(userManager.updateUserVehicle(new Vehicle(matricula, nombre, "eléctrico", consumo100Km))).not.toThrowError();
+    expect(userManager.getUserVehicle(matricula)).not.toThrowError();
+    expect(await userManager.updateUserVehicle(new Vehicle(matricula, nombre, "eléctrico", consumo100Km))).not.toThrowError();
+
+    // delete what is done
+    await getUserManager().deleteVehicle(matricula).catch(() => {})
+});
+
+// E03 - Inválido
+// Given: hay un vehículo con los datos {“nombre”: “nave galáctica”, “tipo-motor”: “combustión”, “consumo-100-km”: “5”} almacenado
+// When: se intenta modificar el tipo de motor del vehículo con el nombre ‘vehículo inexistente’ a eléctrico
+//
+
+test('updateVehicle_UserRegisteredDBAvailableValidInputs_UpdateVehicle', async () => {
+    const email: string = 'edu.jose@gmail.com',
+        password: string = 'aS0@28Y?';
+
+    const matricula: string = "1212XXJ",
+        nombre: string = "nave galáctica",
+        tipoMotor: string = "combustión",
+        consumo100Km: number = 5;
+
+    // tests methods
+    const userManager = getUserManager()
+    await userManager.logIn(email, password);
+    await getUserManager().deleteVehicle(matricula).catch(() => {})
+    await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km).catch(() => {});
+
+    expect(userManager.getUserVehicle("1212XXX")).rejects.toThrowError('Vehicle not found');
+    expect(userManager.updateUserVehicle(new Vehicle("1212XXX", "vehiculo inexistente", "eléctrico", consumo100Km))).rejects.toThrowError('Invalid Vehicle');
 
     // delete what is done
     await getUserManager().deleteVehicle(matricula).catch(() => {})
