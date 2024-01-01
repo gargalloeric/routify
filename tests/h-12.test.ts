bd414093e82afc673aa5a0e97d949631b1a97b86
mehdi <mehdigassabarsa@gmail.com>
2024-01-01 23:50:29 +0100

    feat(it05h12): completed implementation all tests pass

--- tests/h-12.test.ts
+++ tests/h-12.test.ts
@@ -22,8 +22,8 @@ test('updateVehicle_UserRegisteredDBAvailableValidInputs_UpdateVehicle', async (
     await getUserManager().deleteVehicle(matricula).catch(() => {})
     await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km).catch(() => {});
 
-    expect(userManager.getUserVehicle(matricula)).not.toThrowError();
-    expect(await userManager.updateUserVehicle(new Vehicle(matricula, nombre, "eléctrico", consumo100Km))).not.toThrowError();
+    const vehicle : Vehicle = userManager.getUserVehicle(matricula);
+    expect(() => userManager.updateUserVehicle(new Vehicle(vehicle.matricula, vehicle.nombre, "eléctrico", vehicle.consumo100Km))).not.toThrowError('Invalid Vehicle');
 
     // delete what is done
     await getUserManager().deleteVehicle(matricula).catch(() => {})
@@ -32,9 +32,9 @@ test('updateVehicle_UserRegisteredDBAvailableValidInputs_UpdateVehicle', async (
 // E03 - Inválido
 // Given: hay un vehículo con los datos {“nombre”: “nave galáctica”, “tipo-motor”: “combustión”, “consumo-100-km”: “5”} almacenado
 // When: se intenta modificar el tipo de motor del vehículo con el nombre ‘vehículo inexistente’ a eléctrico
-//
+// Then: se lanza la excepción InvalidVehicleException.
 
-test('updateVehicle_UserRegisteredDBAvailableValidInputs_UpdateVehicle', async () => {
+test('updateVehicle_UserRegisteredDBAvailableInValidInputs_ThrowVehicleNotFoundException', async () => {
     const email: string = 'edu.jose@gmail.com',
         password: string = 'aS0@28Y?';
 
@@ -49,8 +49,7 @@ test('updateVehicle_UserRegisteredDBAvailableValidInputs_UpdateVehicle', async (
     await getUserManager().deleteVehicle(matricula).catch(() => {})
     await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km).catch(() => {});
 
-    expect(userManager.getUserVehicle("1212XXX")).rejects.toThrowError('Vehicle not found');
-    expect(userManager.updateUserVehicle(new Vehicle("1212XXX", "vehiculo inexistente", "eléctrico", consumo100Km))).rejects.toThrowError('Invalid Vehicle');
+    await expect(userManager.updateUserVehicle(new Vehicle("1212XXX", "vehiculo inexistente", "eléctrico", consumo100Km))).rejects.toThrowError('Invalid Vehicle');
 
     // delete what is done
     await getUserManager().deleteVehicle(matricula).catch(() => {})
