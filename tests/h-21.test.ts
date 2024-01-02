7b8fba8f4f2203c2eb26b5bbb631da3c50b7b2e2
mehdi <mehdigassabarsa@gmail.com>
2024-01-02 14:29:49 +0100

    test(it05h21): completed accptance test e02

--- tests/h-21.test.ts
+++ tests/h-21.test.ts
@@ -27,4 +27,25 @@ test('defaultVehicle_UserRegisteredDBAvailableVehicleAlreadyDefault_ThrowVehicle
 
     // delete what is done
     await getUserManager().deleteVehicle(matricula).catch(() => {})
-})
+});
+
+test('defaultVehicle_UserRegisteredDBAvailableVehicleNotDefault_DefaultVehicle', async () => {
+    const email: string = 'edu.jose@gmail.com',
+        password: string = 'aS0@28Y?';
+
+    const matricula: string = "1111XXX",
+        nombre: string = "nave galáctica",
+        tipoMotor: string = "combustión",
+        consumo100Km: number = 5;
+
+    // tests methods
+    const userManager = getUserManager()
+    await userManager.logIn(email, password);
+    await getUserManager().deleteVehicle(matricula).catch(() => {})
+    await userManager.registerVehicle(matricula, nombre, tipoMotor, consumo100Km).catch(() => {})
+
+    await expect(userManager.setDefaultVehicle(matricula)).not.toThrowError();
+
+    // delete what is done
+    await getUserManager().deleteVehicle(matricula).catch(() => {})
+});
