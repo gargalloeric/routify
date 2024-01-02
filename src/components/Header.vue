c65cfa928156a9f9f22ba3018274379f8908e4a9
elbence <juanjopc08@gmail.com>
2024-01-02 12:49:42 +0100

    feat(it05h03): interfaces base implementation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,7 +18,7 @@
         <ul
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
-            <a @click="router.push({path: '/'})"
+            <a @click="handleUserButton('/')"
               class="cursor-pointer block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
               aria-current="page">Home</a>
           </li>
@@ -38,9 +38,9 @@
               Mis Lugares</a>
           </li>
           <li>
-            <a @click="handleUserButton('/notFound')"
-               class="cursor-pointer block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-              Profile</a>
+            <a @click="logOut"
+               class="cursor-pointer block py-2 px-3 text-red-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+              LogOut</a>
           </li>
         </ul>
       </div>
@@ -65,6 +65,11 @@ import {getUserManager} from "../services/UserManager.ts";
     } else {
       router.push({path: '/logIn'})
     }
+  }
+
+  function logOut() {
+    getUserManager().logOut()
+    router.push({path: '/logIn'})
   }
 </script>
 
